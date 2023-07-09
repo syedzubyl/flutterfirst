acfd8373ef48bd1c23c8fcdc35d3500dbb63c423
syedzubyl <syedzubyl96@gmail.com>
2023-07-09 19:20:34 +0530

    commit syed

--- lib/hello.dart
+++ lib/hello.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
-
-class HelloScreen extends StatelessWidget {
-  const HelloScreen({Key? key}) : super(key: key);
+class hello extends StatelessWidget {
+  const hello({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Hello"),
+    return  Scaffold(
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(height: 500,
+            width: 500,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/bg.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+
+            ),
+          )
+
+        ],
       ),
-    );
+      );
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:syed/hello.dart';
 
 void main(){
-  runApp(const HelloApp());
+  runApp(const MyApp());
 }
-class HelloApp extends StatelessWidget {
-  const HelloApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Directionality(
-      textDirection: TextDirection.ltr,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hello Flutter',
-        home: HelloScreen(),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo',
+      home: hello(),
     );
   }
 }
